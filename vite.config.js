30deb2f651b44374db12a7d4983aa2fda05a9a3c
alexanderliberacion-cmd <alexanderliberacion@gmail.com>
2026-06-20 18:19:46 +0200

    Comentarios y ordenamiento

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/appTareas2/',
+  base: '/Planificador-de-tareas/',
   plugins: [
     react(),
     tailwindcss(),
